83e587b8b939b9e4e6e81d22a756d3119cdaba75
Sandra <myszmilo2@gmail.com>
2019-03-15 14:57:28 +0100

    init

---
+++ Crypto/Cipher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    class Cipher
+    {
+        private char[] plainText;
+        private string result;
+        private string[] key;
+        private char[][] codeTable;
+
+        Cipher(string initial, string key){
+            this.plainText = initial.Replace(" ", "").Split('');
+            this.result = "";
+            this.key=key.Split('-');
+
+        }
+
+        public void code()
+        {
+
+
+        }
+
+        public
+
+    }
+}
